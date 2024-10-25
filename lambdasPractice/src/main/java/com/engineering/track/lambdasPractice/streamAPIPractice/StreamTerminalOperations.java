0b1e85b09193a7480b0ad01526cf60206bf28302
hp <hp@DESKTOP-6TOVI0A>
2024-10-25 20:53:04 +0530

    Add class

---
+++ lambdasPractice/src/main/java/com/engineering/track/lambdasPractice/streamAPIPractice/StreamTerminalOperations.java
@@ -0,0 +1,10 @@
+package com.engineering.track.lambdasPractice.streamAPIPractice;
+
+public class StreamTerminalOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
